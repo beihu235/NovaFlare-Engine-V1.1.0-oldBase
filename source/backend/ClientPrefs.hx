71ca147bf97bfcbda993bfe8aeeb5b34e756d91b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-19 22:08:19 +0800

    Add files via upload

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -85,7 +85,9 @@ class SaveVariables {
 	public var hitboxLocation:String = 'Bottom';
 	public var hitboxalpha:Float = 0.2;
 	public var VirtualPadAlpha:Float = 0.75;
-
+    public var AndriodUpdateFirstCheck = false; //check for update new assets
+    
+    
 	public function new()
 	{
 		//Why does haxe needs this again?
