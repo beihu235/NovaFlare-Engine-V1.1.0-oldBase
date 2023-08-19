13d65a405a169096b46db53feb7dea370b7ae55b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-19 20:53:57 +0800

    Add files via upload

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -18,13 +18,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 			'If checked, your notes get centered.',
 			'middleScroll',
 			'bool');
-		addOption(option);
-		
-		var option:Option = new Option('Filp Chart',
-			'If checked, filp chart for playing',
-			'filpChart',
-			'bool');
-		addOption(option);
+		addOption(option);				
 
 		var option:Option = new Option('Opponent Notes',
 			'If unchecked, opponent notes get hidden.',
