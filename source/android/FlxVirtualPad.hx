ff66fd6e1848b3c4c0d2997f1430e15966ef4da0
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-25 22:29:51 +0800

    Add files via upload

--- source/android/FlxVirtualPad.hx
+++ source/android/FlxVirtualPad.hx
@@ -143,7 +143,7 @@ class FlxVirtualPad extends FlxSpriteGroup {
 				dPad.add(add(buttonLeft2 = createButton(42 * 3, FlxG.height - 165 * 3, 44 * 3, 127, "left", 0xFF00FF)));
 				dPad.add(add(buttonRight2 = createButton(42 * 3, FlxG.height - 125 * 3, 44 * 3, 127, "right", 0xFF0000)));				
 			case MainMenuState:
-				dPad.add(add(buttonUp = createButton(FlxG.width - 86 * 3, FlxG.height - 85 * 3, 44 * 3, 127, "up", 0x00FF00)));
+				dPad.add(add(buttonUp = createButton(FlxG.width - 44 * 3, FlxG.height - 85 * 3, 44 * 3, 127, "up", 0x00FF00)));
 				dPad.add(add(buttonDown = createButton(FlxG.width - 44 * 3, FlxG.height - 85 * 3, 44 * 3, 127, "down", 0x00FFFF)));		
             case NoteSplashDebugState:
 			    //orgAlpha = 0.75;
