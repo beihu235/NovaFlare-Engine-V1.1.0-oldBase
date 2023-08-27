9a1cda7c496eeeb5a0cc9067dc68cb537cc7c6d3
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-27 20:40:17 +0800

    Add files via upload

--- source/substates/OSTSubstate.hx
+++ source/substates/OSTSubstate.hx
@@ -48,14 +48,31 @@ import flixel.ui.FlxButton;
 class OSTSubstate extends MusicBeatSubstate
 {
     var waveformVoiceSprite:FlxSprite;
-    public static var waveFormMainSprite:FlxSprite;
     var logoBl:FlxSprite;
     var bpm:Float = 0;
     public static var vocals:FlxSound;
     var songVoice:Bool = false;
+    
+    public var camGame:FlxCamera;
+	public var camHUD:FlxCamera;
+	public var camLogo:FlxCamera;
+	
 	public function new(needVoices:Bool,songBpm:Float)
 	{
 		super();		
+		
+		camGame = new FlxCamera();
+		camHUD = new FlxCamera();
+		camLogo = new FlxCamera();
+		camLogo.bgColor.alpha = 0;
+		camHUD.bgColor.alpha = 0;
+
+		FlxG.cameras.reset(camGame);
+		FlxG.cameras.add(camLogo, false);
+		FlxG.cameras.add(camHUD, false);
+		FlxG.cameras.setDefaultDrawTarget(camGame, true);
+		
+		camLogo.offset.x = -320;
 		
 		bpm = songBpm;		
 		
@@ -87,24 +104,17 @@ class OSTSubstate extends MusicBeatSubstate
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
 		logoBl.animation.play('bump');
 		logoBl.centerOrigin();
-		//logoBl.scale.x = 0.5;
-		//logoBl.scale.y = 0.5;
+		logoBl.scale.x = 0.75;
+		logoBl.scale.y = 0.75;
 		logoBl.centerOffsets();
 		logoBl.updateHitbox();
 		add(logoBl);
-		logoBl.x = 320 - logoBl.frameWidth / 2;
-		logoBl.y = 360 - logoBl.frameHeight / 2;
+		logoBl.cameras = [camLogo];
+		logoBl.screenCenter(XY);
 		
-		waveformVoiceSprite = new FlxSprite(1280 - 400 - 50, 50).makeGraphic(400, 100, 0xFF000000);
+		waveformVoiceSprite = new FlxSprite(1280 - 640, 50).makeGraphic(640 - 50, 100, 0xFF000000);
 		waveformVoiceSprite.alpha = 0.5;
 		add(waveformVoiceSprite);
-				
-		waveFormMainSprite = new FlxSprite(1280 - 400 - 50, 50 + 200).makeGraphic(400, 100, 0xFF000000);
-		waveFormMainSprite.alpha = 0.5;
-		add(waveFormMainSprite);
-		
-		
-	
 	    
 	}
     
@@ -117,7 +127,6 @@ class OSTSubstate extends MusicBeatSubstate
 		
 		
 		updateVoiceWaveform();		
-		updateMainWaveform();
 		
 		SoundTime = FlxG.sound.music.time / 1000;
         BeatTime = 60 / bpm;
@@ -157,7 +166,7 @@ class OSTSubstate extends MusicBeatSubstate
 	
 	    var flashGFX = FlxSpriteUtil.flashGfx;
 		
-		var _rect = new Rectangle(0, 0, 400, 100);
+		var _rect = new Rectangle(0, 0, 640 - 50, 100);
 		//var _temprect = new Rectangle(0, 0, 0, 0);
 		var midx = 100 / 2;
 	
@@ -236,105 +245,12 @@ class OSTSubstate extends MusicBeatSubstate
 			
 			index++;
 			rows++;
-			if (render > 400 - 1) break;
+			if (render > (640 - 50 - 1)) break;
 		}
 		
 		flashGFX.endFill(); 
 		waveformVoiceSprite.pixels.draw(FlxSpriteUtil.flashGfxSprite);
 		waveformVoiceSprite.pixels.unlock(); 
-		
-		return;
-	}	
-	
-	public static function updateMainWaveform() {
-	
-	    var flashGFX = FlxSpriteUtil.flashGfx;
-		
-		var _rect = new Rectangle(0, 0, 400, 100);
-		//var _temprect = new Rectangle(0, 0, 0, 0);
-		var midx = 100 / 2;
-	
-	    waveFormMainSprite.pixels.lock();
-		waveFormMainSprite.pixels.fillRect(_rect, 0xFF000000);
-
-		FlxSpriteUtil.beginDraw(0xFFFFFFFF);
-		
-		
-		var snd = FlxG.sound.music;
-
-		var currentTime = snd.time;
-		
-		var buffer = snd._sound.__buffer;
-		var bytes = buffer.data.buffer;
-		
-		var length = bytes.length - 1;
-		var khz = (buffer.sampleRate / 1000);
-		var channels = buffer.channels;
-		var stereo = channels > 1;
-		
-		var index = Math.floor(currentTime * khz);
-		var samples = 720;//Math.floor((currentTime + (((60 / Conductor.bpm) * 1000 / 4) * 16)) * khz - index);
-		var samplesPerRow = samples / 720;
-
-		var lmin:Float = 0;
-		var lmax:Float = 0;
-		
-		var rmin:Float = 0;
-		var rmax:Float = 0;
-
-		var rows = 0;
-		var render = 0;
-		var prevRows = 0;
-		
-		while (index < length) {
-			if (index >= 0) {
-				var byte = bytes.getUInt16(index * channels * 2);
-
-				if (byte > 65535 / 2) byte -= 65535;
-
-				var sample = (byte / 65535);
-
-				if (sample > 0) {
-					if (sample > lmax) lmax = sample;
-				} else if (sample < 0) {
-					if (sample < lmin) lmin = sample;
-				}
-
-				if (stereo) {
-					var byte = bytes.getUInt16((index * channels * 2) + 2);
-
-					if (byte > 65535 / 2) byte -= 65535;
-
-					var sample = (byte / 65535);
-
-					if (sample > 0) {
-						if (sample > rmax) rmax = sample;
-					} else if (sample < 0) {
-						if (sample < rmin) rmin = sample;
-					}
-				}
-			}
-			
-			if (rows - prevRows >= samplesPerRow) {
-				prevRows = rows + ((rows - prevRows) - 1);
-				
-				flashGFX.drawRect(render, 100, 1, -(rmax - rmin) * midx * 2);
-				//flashGFX.drawRect(midx + (rmin * midx * 2), render, (rmax - rmin) * midx * 2, 1);
-				
-				
-				
-				lmin = lmax = rmin = rmax = 0;
-				render++;
-			}
-			
-			index++;
-			rows++;
-			if (render > 400 - 1) break;
-		}
-		
-		flashGFX.endFill(); 
-		waveFormMainSprite.pixels.draw(FlxSpriteUtil.flashGfxSprite);
-		waveFormMainSprite.pixels.unlock(); 
 		
 		return;
 	}	
