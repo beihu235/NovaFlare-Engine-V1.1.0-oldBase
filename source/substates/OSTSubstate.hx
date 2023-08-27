7989eb3ba2d760177b5ed389098e535817a6ad2e
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-27 09:15:06 +0800

    Add files via upload

--- source/substates/OSTSubstate.hx
+++ source/substates/OSTSubstate.hx
@@ -47,39 +47,7 @@ import flixel.ui.FlxButton;
 
 class OSTSubstate extends MusicBeatSubstate
 {
-    public static var vocals:FlxSound = null;
-    var left:FlxSprite;
-    var flashSpr:FlxSprite;
-    var _rect:Rectangle;
-    var _temprect:Rectangle;
-    
-    var snd = FlxG.sound.music;
-
-		var currentTime:Float = 0;
-		
-		var buffer:AudioBuffer;
-		var bytes:Bytes = sound._sound.__buffer.data.toBytes();
-		
-		var byteLength:Float = 0;
-		var khz:Float = 0;
-		var channels:Float = 0;
-		var stereo:Float = 0;
-		
-		var index:Float = 0;
-		var samples:Float = 0;//Math.floor((currentTime + (((60 / Conductor.bpm) * 1000 / 4) * 16)) * khz - index);
-		var samplesPerRow:Float = 0;
-
-		var lmin:Float = 0;
-		var lmax:Float = 0;
-
-		var rmin:Float = 0;
-		var rmax:Float = 0;
-
-		var rows:Float = 0;
-		var render:Int = 0;
-		var prevRows:Float = 0;
-        var midx:Float = 0;
-    
+    var waveformSprite:FlxSprite;
 	public function new(needVoices:Bool,bpm:Float)
 	{
 		super();				
@@ -96,44 +64,9 @@ class OSTSubstate extends MusicBeatSubstate
 		vocals.looped = true;
 		vocals.volume = 0.7;		
 		
-		left = new FlxSprite().makeGraphic(1280, 720, 0xFF000000, false, "lchannelw");
-		left.alpha = 0.5;
-		add(left);
+		waveformSprite = new FlxSprite().makeGraphic(1280, 720, 0x00FFFFFF);
+		add(waveformSprite);
 		
-		flashSpr = new FlxSprite();
-		flashGFX = flashSpr.graphics;
-		
-		currentTime = snd.time;
-		
-		buffer = snd._sound.__buffer;
-		bytes = buffer.data.buffer;
-		
-		byteLength = bytes.byteLength - 1;
-		khz = (buffer.sampleRate / 1000);
-		channels = buffer.channels;
-		stereo = channels > 1;
-		
-		index = Math.floor(currentTime * khz);
-		samples = 720;//Math.floor((currentTime + (((60 / Conductor.bpm) * 1000 / 4) * 16)) * khz - index);
-		samplesPerRow = samples / 720;
-
-		lmin = 0;
-		lmax = 0;
-
-		rmin = 0;
-		rmax = 0;
-
-		rows = 0;
-		render = 0;
-		prevRows = 0;
-		
-		//game.add(right);
-		
-		//flashGFX = FlxSpriteUtil.flashGfx;
-		
-		_rect = new Rectangle(0, 0, 1280, 720);
-		_temprect = new Rectangle(0, 0, 0, 0);
-		midx = 720 / 2;
 		
 	}
 
@@ -157,38 +90,171 @@ class OSTSubstate extends MusicBeatSubstate
 			#end
 		}
 		
-		left.pixels.lock();
-		left.pixels.fillRect(_rect, 0xFF000000);
+		updateWaveform();
 		
-		//right.pixels.lock();
-		//right.pixels.fillRect(_rect, 0xFF000000);
+		
+		
+		super.update(elapsed);
+	}
+	
+	var waveformPrinted:Bool = true;
+	var wavData:Array<Array<Array<Float>>> = [[[0], [0]], [[0], [0]]];
 
-		FlxSpriteUtil.beginDraw(0xFFFFFFFF);
-		flashGFX.clear(); flashGFX.beginFill(0xFFFFFF, 1);
+	var lastWaveformHeight:Int = 0;
+	function updateWaveform() {
+		if(waveformPrinted) {
+			var width:Int = 1280;
+			var height:Int = 720;
+			if(lastWaveformHeight != height && waveformSprite.pixels != null)
+			{
+				waveformSprite.pixels.dispose();
+				waveformSprite.pixels.disposeImage();
+				waveformSprite.makeGraphic(width, height, 0x00FFFFFF);
+				lastWaveformHeight = height;
+			}
+			waveformSprite.pixels.fillRect(new Rectangle(0, 0, width, height), 0x00FFFFFF);
+		}
+		waveformPrinted = false;
+
+		if(!FlxG.save.data.chart_waveformInst && !FlxG.save.data.chart_waveformVoices) {
+			//trace('Epic fail on the waveform lol');
+			return;
+		}
 		
+		wavData[0][0] = [];
+		wavData[0][1] = [];
+		wavData[1][0] = [];
+		wavData[1][1] = [];
 		
+		var steps:Int = Math.round(getSectionBeats() * 4);
+		var st:Float = sectionStartTime();
+		var et:Float = st + (Conductor.stepCrochet * steps);
 		
-		while (index < byteLength) {
+		if (FlxG.save.data.chart_waveformInst) {
+			var sound:FlxSound = FlxG.sound.music;
+			if (sound._sound != null && sound._sound.__buffer != null) {
+				var bytes:Bytes = sound._sound.__buffer.data.toBytes();
+				
+				wavData = waveformData(
+					sound._sound.__buffer,
+					bytes,
+					st,
+					et,
+					1,
+					wavData,
+					Std.int(gridBG.height)
+				);
+			}
+		}
+		
+		if (FlxG.save.data.chart_waveformVoices) {
+			var sound:FlxSound = vocals;
+			if (sound._sound != null && sound._sound.__buffer != null) {
+				var bytes:Bytes = sound._sound.__buffer.data.toBytes();
+				
+				wavData = waveformData(
+					sound._sound.__buffer,
+					bytes,
+					st,
+					et,
+					1,
+					wavData,
+					Std.int(gridBG.height)
+				);
+			}
+		}
+		
+		// Draws
+		var gSize:Int = Std.int(GRID_SIZE * 8);
+		var hSize:Int = Std.int(gSize / 2);
+		
+		var lmin:Float = 0;
+		var lmax:Float = 0;
+		
+		var rmin:Float = 0;
+		var rmax:Float = 0;
+		
+		var size:Float = 1;
+		
+		var leftLength:Int = (
+			wavData[0][0].length > wavData[0][1].length ? wavData[0][0].length : wavData[0][1].length
+		);
+		
+		var rightLength:Int = (
+			wavData[1][0].length > wavData[1][1].length ? wavData[1][0].length : wavData[1][1].length
+		);
+		
+		var length:Int = leftLength > rightLength ? leftLength : rightLength;
+		
+		var index:Int;
+		for (i in 0...length) {
+			index = i;
+			
+			lmin = FlxMath.bound(((index < wavData[0][0].length && index >= 0) ? wavData[0][0][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
+			lmax = FlxMath.bound(((index < wavData[0][1].length && index >= 0) ? wavData[0][1][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
+			
+			rmin = FlxMath.bound(((index < wavData[1][0].length && index >= 0) ? wavData[1][0][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
+			rmax = FlxMath.bound(((index < wavData[1][1].length && index >= 0) ? wavData[1][1][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
+			
+			waveformSprite.pixels.fillRect(new Rectangle(hSize - (lmin + rmin), i * size, (lmin + rmin) + (lmax + rmax), size), FlxColor.BLUE);
+		}
+		
+		waveformPrinted = true;
+	}
+
+	function waveformData(buffer:AudioBuffer, bytes:Bytes, time:Float, endTime:Float, multiply:Float = 1, ?array:Array<Array<Array<Float>>>, ?steps:Float):Array<Array<Array<Float>>>
+	{
+		#if (lime_cffi && !macro)
+		if (buffer == null || buffer.data == null) return [[[0], [0]], [[0], [0]]];
+		
+		var khz:Float = (buffer.sampleRate / 1000);
+		var channels:Int = buffer.channels;
+		
+		var index:Int = Std.int(time * khz);
+		
+		var samples:Float = ((endTime - time) * khz);
+		
+		if (steps == null) steps = 1280;
+		
+		var samplesPerRow:Float = samples / steps;
+		var samplesPerRowI:Int = Std.int(samplesPerRow);
+		
+		var gotIndex:Int = 0;
+		
+		var lmin:Float = 0;
+		var lmax:Float = 0;
+		
+		var rmin:Float = 0;
+		var rmax:Float = 0;
+		
+		var rows:Float = 0;
+		
+		var simpleSample:Bool = true;//samples > 17200;
+		var v1:Bool = false;
+		
+		if (array == null) array = [[[0], [0]], [[0], [0]]];
+		
+		while (index < (bytes.length - 1)) {
 			if (index >= 0) {
-				var byte = bytes.getUInt16(index * channels * 2);
-
+				var byte:Int = bytes.getUInt16(index * channels * 2);
+				
 				if (byte > 65535 / 2) byte -= 65535;
-
-				var sample = (byte / 65535);
-
+				
+				var sample:Float = (byte / 65535);
+				
 				if (sample > 0) {
 					if (sample > lmax) lmax = sample;
 				} else if (sample < 0) {
 					if (sample < lmin) lmin = sample;
 				}
-
-				if (stereo) {
-					var byte:Float = bytes.getUInt16((index * channels * 2) + 2);
-
+				
+				if (channels >= 2) {
+					byte = bytes.getUInt16((index * channels * 2) + 2);
+					
 					if (byte > 65535 / 2) byte -= 65535;
-
-					var sample:Float = (byte / 65535);
-
+					
+					sample = (byte / 65535);
+					
 					if (sample > 0) {
 						if (sample > rmax) rmax = sample;
 					} else if (sample < 0) {
@@ -197,30 +263,56 @@ class OSTSubstate extends MusicBeatSubstate
 				}
 			}
 			
-			if (rows - prevRows >= samplesPerRow) {
-				prevRows = rows + ((rows - prevRows) - 1);
+			v1 = samplesPerRowI > 0 ? (index % samplesPerRowI == 0) : false;
+			while (simpleSample ? v1 : rows >= samplesPerRow) {
+				v1 = false;
+				rows -= samplesPerRow;
 				
-				flashGFX.drawRect(render, midx + (rmin * midx * 2), 1, (rmax - rmin) * midx * 2);
-				//flashGFX2.drawRect(midx + (rmin * midx * 2), render, (rmax - rmin) * midx * 2, 1);
+				gotIndex++;
 				
+				var lRMin:Float = Math.abs(lmin) * multiply;
+				var lRMax:Float = lmax * multiply;
 				
+				var rRMin:Float = Math.abs(rmin) * multiply;
+				var rRMax:Float = rmax * multiply;
 				
-				lmin = lmax = rmin = rmax = 0;
-				render++;
+				if (gotIndex > array[0][0].length) array[0][0].push(lRMin);
+					else array[0][0][gotIndex - 1] = array[0][0][gotIndex - 1] + lRMin;
+				
+				if (gotIndex > array[0][1].length) array[0][1].push(lRMax);
+					else array[0][1][gotIndex - 1] = array[0][1][gotIndex - 1] + lRMax;
+				
+				if (channels >= 2) {
+					if (gotIndex > array[1][0].length) array[1][0].push(rRMin);
+						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + rRMin;
+					
+					if (gotIndex > array[1][1].length) array[1][1].push(rRMax);
+						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + rRMax;
+				}
+				else {
+					if (gotIndex > array[1][0].length) array[1][0].push(lRMin);
+						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + lRMin;
+					
+					if (gotIndex > array[1][1].length) array[1][1].push(lRMax);
+						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + lRMax;
+				}
+				
+				lmin = 0;
+				lmax = 0;
+				
+				rmin = 0;
+				rmax = 0;
 			}
 			
 			index++;
 			rows++;
-			if (render > 720-1) break;
+			if(gotIndex > steps) break;
 		}
 		
-		flashGFX.endFill();
-		left.pixels.draw(flashSpr);
-		left.pixels.unlock();
-		
-		return;
-		
-		super.update(elapsed);
+		return array;
+		#else
+		return [[[0], [0]], [[0], [0]]];
+		#end
 	}
 
 	public static function destroyVocals() {
