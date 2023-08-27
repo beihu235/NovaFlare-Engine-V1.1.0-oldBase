1871eea96b449763601869f2d6c0a6899fe006b9
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-27 10:21:06 +0800

    Add files via upload

--- source/substates/OSTSubstate.hx
+++ source/substates/OSTSubstate.hx
@@ -65,7 +65,16 @@ class OSTSubstate extends MusicBeatSubstate
 		vocals.looped = true;
 		vocals.volume = 0.7;		
 		
-		waveformSprite = new FlxSprite().makeGraphic(1280, 720, 0x00FFFFFF);
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+		bg.scrollFactor.set(0,0);
+		bg.setGraphicSize(Std.int(bg.width));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		add(bg);
+		
+		waveformSprite = new FlxSprite().makeGraphic(1280, 720, 0xFF000000);
+		waveformSprite.alpha = 0.5;.
 		add(waveformSprite);
 		
 		
@@ -91,7 +100,7 @@ class OSTSubstate extends MusicBeatSubstate
 			#end
 		}
 		
-		updateWaveform();
+		waveformData();
 		
 		
 		
@@ -205,7 +214,6 @@ class OSTSubstate extends MusicBeatSubstate
 
 	function waveformData(buffer:AudioBuffer, bytes:Bytes, time:Float, endTime:Float, multiply:Float = 1, ?array:Array<Array<Array<Float>>>, ?steps:Float):Array<Array<Array<Float>>>
 	{
-		#if (lime_cffi && !macro)
 		if (buffer == null || buffer.data == null) return [[[0], [0]], [[0], [0]]];
 		
 		var khz:Float = (buffer.sampleRate / 1000);
@@ -230,32 +238,32 @@ class OSTSubstate extends MusicBeatSubstate
 		
 		var rows:Float = 0;
 		
+		var midx = 720 / 2;
+		
 		var simpleSample:Bool = true;//samples > 17200;
 		var v1:Bool = false;
 		
-		if (array == null) array = [[[0], [0]], [[0], [0]]];
-		
-		while (index < (bytes.length - 1)) {
+		while (index < length) {
 			if (index >= 0) {
-				var byte:Int = bytes.getUInt16(index * channels * 2);
-				
+				var byte = bytes.getUInt16(index * channels * 2);
+
 				if (byte > 65535 / 2) byte -= 65535;
-				
-				var sample:Float = (byte / 65535);
-				
+
+				var sample = (byte / 65535);
+
 				if (sample > 0) {
 					if (sample > lmax) lmax = sample;
 				} else if (sample < 0) {
 					if (sample < lmin) lmin = sample;
 				}
-				
-				if (channels >= 2) {
-					byte = bytes.getUInt16((index * channels * 2) + 2);
-					
+
+				if (stereo) {
+					var byte = bytes.getUInt16((index * channels * 2) + 2);
+
 					if (byte > 65535 / 2) byte -= 65535;
-					
-					sample = (byte / 65535);
-					
+
+					var sample = (byte / 65535);
+
 					if (sample > 0) {
 						if (sample > rmax) rmax = sample;
 					} else if (sample < 0) {
@@ -264,56 +272,31 @@ class OSTSubstate extends MusicBeatSubstate
 				}
 			}
 			
-			v1 = samplesPerRowI > 0 ? (index % samplesPerRowI == 0) : false;
-			while (simpleSample ? v1 : rows >= samplesPerRow) {
-				v1 = false;
-				rows -= samplesPerRow;
+			if (rows - prevRows >= samplesPerRow) {
+				prevRows = rows + ((rows - prevRows) - 1);
 				
-				gotIndex++;
+				waveformSprite.drawRect(render, midx + (rmin * midx * 2), 1, (rmax - rmin) * midx * 2);
+				//flashGFX2.drawRect(midx + (rmin * midx * 2), render, (rmax - rmin) * midx * 2, 1);
 				
-				var lRMin:Float = Math.abs(lmin) * multiply;
-				var lRMax:Float = lmax * multiply;
 				
-				var rRMin:Float = Math.abs(rmin) * multiply;
-				var rRMax:Float = rmax * multiply;
 				
-				if (gotIndex > array[0][0].length) array[0][0].push(lRMin);
-					else array[0][0][gotIndex - 1] = array[0][0][gotIndex - 1] + lRMin;
-				
-				if (gotIndex > array[0][1].length) array[0][1].push(lRMax);
-					else array[0][1][gotIndex - 1] = array[0][1][gotIndex - 1] + lRMax;
-				
-				if (channels >= 2) {
-					if (gotIndex > array[1][0].length) array[1][0].push(rRMin);
-						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + rRMin;
-					
-					if (gotIndex > array[1][1].length) array[1][1].push(rRMax);
-						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + rRMax;
-				}
-				else {
-					if (gotIndex > array[1][0].length) array[1][0].push(lRMin);
-						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + lRMin;
-					
-					if (gotIndex > array[1][1].length) array[1][1].push(lRMax);
-						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + lRMax;
-				}
-				
-				lmin = 0;
-				lmax = 0;
-				
-				rmin = 0;
-				rmax = 0;
+				lmin = lmax = rmin = rmax = 0;
+				render++;
 			}
 			
 			index++;
 			rows++;
-			if(gotIndex > 1280) break;
+			if (render > 1280) break;
 		}
 		
-		return array;
-		#else
-		return [[[0], [0]], [[0], [0]]];
-		#end
+		waveformSprite.endFill();
+		//waveformSprite.pixels.draw(flashSpr2);
+		waveformSprite.pixels.unlock();
+		//left.dirty = true;
+		
+		return;
+		
+		
 	}
 
 	public static function destroyVocals() {
