485de6ea41e0307e4ab2f239ac31b0c80c7f2136
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-28 19:44:18 +0800

    Add files via upload

---
+++ source/substates/CustomFadeTransition.hx
@@ -0,0 +1,144 @@
+package backend;
+
+import flixel.util.FlxGradient;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.FlxSubState;
+import flixel.FlxSprite;
+import openfl.utils.Assets;
+import flixel.FlxObject;
+
+class CustomFadeTransition extends MusicBeatSubstate {
+	public static var finishCallback:Void->Void;
+	private var leTween:FlxTween = null;
+	public static var nextCamera:FlxCamera;
+	var isTransIn:Bool = false;
+	var transBlack:FlxSprite;
+	var transGradient:FlxSprite;
+	
+	var loadLeft:FlxSprite;
+	var loadRight:FlxSprite;
+	var WaterMark:FlxText;
+	var EventText:FlxText;
+	
+	var loadLeftTween:FlxTween;
+	var loadRightTween:FlxTween;
+	var EventTextTween:FlxTween;
+	var loadTextTween:FlxTween;
+
+	public function new(duration:Float, isTransIn:Bool) {
+		super();
+
+		this.isTransIn = isTransIn;
+		
+		loadLeft = new FlxSprite(isTransIn ? 0 : -1280, 0).loadGraphic(Paths.image('mainmenu_sprite/loadingL'));
+		loadLeft.scrollFactor.set();
+		loadLeft.antialiasing = ClientPrefs.data.antialiasing;
+		add(loadLeft);
+		loadLeft.scale.set(loadLeft.width / 1280, loadLeft.height / 720);
+		
+		loadRight = new FlxSprite(isTransIn ? 0 : 1280, 0).loadGraphic(Paths.image('mainmenu_sprite/loadingR'));
+		loadRight.scrollFactor.set();
+		loadRight.antialiasing = ClientPrefs.data.antialiasing;
+		add(loadRight);
+		loadRight.scale.set(loadRight.width / 1280, loadRight.height / 720);
+		
+		WaterMark = new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50 * 2, 0, 'NF ENGINE V1.1.0', 50);
+		WaterMark.scrollFactor.set();
+		WaterMark.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 113, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		WaterMark.antialiasing = ClientPrefs.data.antialiasing;
+		add(WaterMark);
+        
+        EventText= new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50, 0, 'LOADING . . . . . . ', 50);
+		EventText.scrollFactor.set();
+		EventText.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 113, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		EventText.antialiasing = ClientPrefs.data.antialiasing;
+		add(EventText);
+		
+		if(!isTransIn) {
+			FlxG.sound.play(Paths.sound('loading_close'));
+			loadLeftTween = FlxTween.tween(loadLeft, {x: 0}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadRightTween = FlxTween.tween(loadRight, {x: 0}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadTextTween = FlxTween.tween(WaterMark, {x: 50}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+			EventTextTween = FlxTween.tween(EventText, {x: 50}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+		} else {
+			FlxG.sound.play(Paths.sound('loading_open'));
+			EventText.text = 'COMPLETED !';
+			
+			loadLeftTween = FlxTween.tween(loadLeft, {x: -1280}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadRightTween = FlxTween.tween(loadRight, {x: 1280}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadTextTween = FlxTween.tween(WaterMark, {x: -1230}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			EventTextTween = FlxTween.tween(EventText, {x: -1230}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			
+		}
+
+		if(nextCamera != null) {
+			loadRight.cameras = [nextCamera];
+			loadLeft.cameras = [nextCamera];
+			WaterMark.cameras = [nextCamera];
+			EventText.cameras = [nextCamera];
+		}
+		nextCamera = null;
+	}
+
+	override function destroy() {
+		if(leTween != null) {
+			finishCallback();
+			leTween.cancel();
+			loadLeftTween.cancel();
+			loadRightTween.cancel();
+			loadTextTween.cancel();
+			EventTextTween.cancel();
+		}
+		super.destroy();
+	}
+}
