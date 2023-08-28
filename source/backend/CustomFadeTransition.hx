df7924fb3d2ad93d7ff2b199e1b4406813d51760
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-28 22:38:38 +0800

    Add files via upload

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -1,6 +1,13 @@
 package backend;
 
 import flixel.util.FlxGradient;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.FlxSubState;
+import flixel.FlxSprite;
+import openfl.utils.Assets;
+import flixel.FlxObject;
 
 class CustomFadeTransition extends MusicBeatSubstate {
 	public static var finishCallback:Void->Void;
@@ -9,73 +16,131 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	var isTransIn:Bool = false;
 	var transBlack:FlxSprite;
 	var transGradient:FlxSprite;
+	
+	var loadLeft:FlxSprite;
+	var loadRight:FlxSprite;
+	var WaterMark:FlxText;
+	var EventText:FlxText;
+	
+	var loadLeftTween:FlxTween;
+	var loadRightTween:FlxTween;
+	var EventTextTween:FlxTween;
+	var loadTextTween:FlxTween;
 
 	public function new(duration:Float, isTransIn:Bool) {
 		super();
 
 		this.isTransIn = isTransIn;
-		var zoom:Float = FlxMath.bound(FlxG.camera.zoom, 0.05, 1);
-		var width:Int = Std.int(FlxG.width / zoom);
-		var height:Int = Std.int(FlxG.height / zoom);
-		transGradient = FlxGradient.createGradientFlxSprite(1, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scale.x = width;
-		transGradient.updateHitbox();
-		transGradient.scrollFactor.set();
-		add(transGradient);
-
-		transBlack = new FlxSprite().makeGraphic(1, height + 400, FlxColor.BLACK);
-		transBlack.scale.x = width;
-		transBlack.updateHitbox();
-		transBlack.scrollFactor.set();
-		add(transBlack);
-
-		transGradient.x -= (width - FlxG.width) / 2;
-		transBlack.x = transGradient.x;
-
-		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
-			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.linear});
-		} else {
-			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
-			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+		
+		loadLeft = new FlxSprite(isTransIn ? 0 : -1280, 0).loadGraphic(Paths.image('mainmenu_sprite/loadingL'));
+		loadLeft.scrollFactor.set();
+		loadLeft.antialiasing = ClientPrefs.data.antialiasing;
+		add(loadLeft);
+		loadLeft.scale.x = loadLeft.width / 1280;
+		loadLeft.scale.y = loadLeft.height / 720;
+		
+		
+		loadRight = new FlxSprite(isTransIn ? 0 : 1280, 0).loadGraphic(Paths.image('mainmenu_sprite/loadingR'));
+		loadRight.scrollFactor.set();
+		loadRight.antialiasing = ClientPrefs.data.antialiasing;		
+		add(loadRight);
+		loadLeft.scale.x = loadLeft.width / 1280;
+		loadLeft.scale.y = loadLeft.height / 720;
+		
+		WaterMark = new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50 * 2, 0, 'NF ENGINE V1.1.0', 50);
+		WaterMark.scrollFactor.set();
+		WaterMark.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 113, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		WaterMark.antialiasing = ClientPrefs.data.antialiasing;
+		add(WaterMark);
+        
+        EventText= new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50, 0, 'LOADING . . . . . . ', 50);
+		EventText.scrollFactor.set();
+		EventText.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 113, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		EventText.antialiasing = ClientPrefs.data.antialiasing;
+		add(EventText);
+		
+		if(!isTransIn) {
+			FlxG.sound.play(Paths.sound('loading_close'));
+			loadLeftTween = FlxTween.tween(loadLeft, {x: 0}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
 						finishCallback();
 					}
 				},
-			ease: FlxEase.linear});
+			ease: FlxEase.expoInOut});
+			
+			loadRightTween = FlxTween.tween(loadRight, {x: 0}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadTextTween = FlxTween.tween(WaterMark, {x: 50}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+			EventTextTween = FlxTween.tween(EventText, {x: 50}, duration, {
+				onComplete: function(twn:FlxTween) {
+					if(finishCallback != null) {
+						finishCallback();
+					}
+				},
+			ease: FlxEase.expoInOut});
+			
+		} else {
+			FlxG.sound.play(Paths.sound('loading_open'));
+			EventText.text = 'COMPLETED !';
+			
+			loadLeftTween = FlxTween.tween(loadLeft, {x: -1280}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadRightTween = FlxTween.tween(loadRight, {x: 1280}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			loadTextTween = FlxTween.tween(WaterMark, {x: -1230}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			EventTextTween = FlxTween.tween(EventText, {x: -1230}, duration, {
+				onComplete: function(twn:FlxTween) {
+					close();
+				},
+			ease: FlxEase.expoInOut});
+			
+			
 		}
 
 		if(nextCamera != null) {
-			transBlack.cameras = [nextCamera];
-			transGradient.cameras = [nextCamera];
+			loadRight.cameras = [nextCamera];
+			loadLeft.cameras = [nextCamera];
+			WaterMark.cameras = [nextCamera];
+			EventText.cameras = [nextCamera];
 		}
 		nextCamera = null;
-	}
-
-	override function update(elapsed:Float) {
-		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
-		} else {
-			transBlack.y = transGradient.y - transBlack.height;
-		}
-		super.update(elapsed);
-		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
-		} else {
-			transBlack.y = transGradient.y - transBlack.height;
-		}
 	}
 
 	override function destroy() {
 		if(leTween != null) {
 			finishCallback();
 			leTween.cancel();
+			loadLeftTween.cancel();
+			loadRightTween.cancel();
+			loadTextTween.cancel();
+			EventTextTween.cancel();
 		}
 		super.destroy();
 	}
