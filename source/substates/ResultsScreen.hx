0141263eff3f85dc5032feab18de99e9c37ac4ca
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-01 21:00:10 +0800

    Add files via upload

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -47,23 +47,19 @@ class ResultsScreen extends MusicBeatSubstate
 		add(graphBG);
 		
 		var what = FlxSpriteUtil.flashGfx;		
-		
 		var _rect = new Rectangle(0, 0, graphWidth, graphHeight);
-		graphBG.pixels.lock();
 		graphBG.pixels.fillRect(_rect, 0xFF000000);
 		FlxSpriteUtil.beginDraw(0xFFFFFFFF);
 	    
 	    var noteSize = 2;
-	    //graphBG.pixels.lock();
+	    graphBG.pixels.lock();
 		for (i in 0...PlayState.rsNoteTime.length){
-		var test = new FlxSprite(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.7 * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2).makeGraphic(noteSize, noteSize, FlxColor.WHITE);
-		var flashGFX2 = test.graphic;
-		//what.drawCircle(, noteSize);		
-		graphBG.pixels.draw(flashGFX2);
-		}
 		
-		//graphBG.pixels.unlock();
-		//what.endFill();
+		what.drawCircle(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.7 * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize);		
+		graphBG.pixels.draw(FlxSpriteUtil.flashGfxSprite);
+		}
+		graphBG.pixels.unlock();
+		what.endFill();
 		
 		graphSizeUp = new FlxSprite(graphBG.x, graphBG.y - 1).makeGraphic(graphWidth - 1, 2, FlxColor.WHITE);
 		graphSizeUp.scrollFactor.set();
