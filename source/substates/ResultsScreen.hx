e93223f6cca8a34dd05aebceff9f8bf151ab97da
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-02 18:48:15 +0800

    Add files via upload

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -85,7 +85,7 @@ class ResultsScreen extends MusicBeatSubstate
 		graphBG.alpha = 0;		
 		add(graphBG);
 		
-		//var noteSpr = FlxSpriteUtil.flashGfx;		
+		var noteSpr = FlxSpriteUtil.flashGfx;		
 		var _rect = new Rectangle(0, 0, graphWidth, graphHeight);
 		graphBG.pixels.fillRect(_rect, 0xFF000000);
 		FlxSpriteUtil.beginDraw(0xFFFFFFFF);
@@ -101,12 +101,13 @@ class ResultsScreen extends MusicBeatSubstate
 		    if (Math.abs(PlayState.rsNoteMs[i]) < 45) color = ColorArray[0];
 		    
 		    if (Math.abs(PlayState.rsNoteMs[i]) < 166){
-    		drawCircle(graphBG,graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * MoveSize * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize, color);
+    		//noteSpr.drawCircle(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * MoveSize * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize, color);
     		}
     		else{
-    	    FlxSpriteUtil.drawCircle(graphBG,graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.8 - noteSize / 2, noteSize, color);		
+    		//noteSpr.drawCircle(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.8 - noteSize / 2, noteSize, color);		
     		}
-		    FlxSpriteUtil.graphBG.pixels.draw(FlxSpriteUtil.flashGfxSprite);
+    	    var test = new FlxSprite(10, 10).makeGraphic(10, 10, FlxColor.BLACK);
+		    graphBG.pixels.draw(test);
 		}
 		
 		var judgeHeight = 2;
