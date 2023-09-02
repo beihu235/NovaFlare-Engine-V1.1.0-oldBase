68e8670dfa0950dc28b095947e330b7be67d4f4a
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-02 19:00:44 +0800

    Add files via upload

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -101,10 +101,10 @@ class ResultsScreen extends MusicBeatSubstate
 		    if (Math.abs(PlayState.rsNoteMs[i]) < 45) color = ColorArray[0];
 		    
 		    if (Math.abs(PlayState.rsNoteMs[i]) < 166){
-    		noteSpr.drawRect(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * MoveSize * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize, noteSize, color);
+    		FlxSpriteUtil.drawRect(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * MoveSize * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize, noteSize, color);
     		}
     		else{
-    		noteSpr.drawRect(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.8 - noteSize / 2, noteSize, noteSize, color);		
+    		FlxSpriteUtil.drawRect(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.8 - noteSize / 2, noteSize, noteSize, color);		
     		}
     		
 		    graphBG.pixels.draw(FlxSpriteUtil.flashGfxSprite);
