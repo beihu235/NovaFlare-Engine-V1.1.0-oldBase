6ef448d75d23b9c7b9b736cf41f519c365f16465
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-02 17:28:01 +0800

    Add files via upload

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -101,7 +101,7 @@ class ResultsScreen extends MusicBeatSubstate
 		    if (Math.abs(PlayState.rsNoteMs[i] < 90)) color = ColorArray[1];
 		    if (Math.abs(PlayState.rsNoteMs[i] < 45)) color = ColorArray[0];
 		    
-		    if (Math.abs(PlayState.rsNoteMs[i] < 166){
+		    if (Math.abs(PlayState.rsNoteMs[i] < 166)){
     		noteSpr.drawCircle(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * MoveSize * (PlayState.rsNoteMs[i] / 166.6) - noteSize / 2, noteSize,color);				   }
     		else{
     		noteSpr.drawCircle(graphWidth * (PlayState.rsNoteTime[i] / PlayState.rsSongLength) - noteSize / 2 , graphHeight * 0.5 + graphHeight * 0.5 * 0.8 - noteSize / 2, noteSize,color);		
