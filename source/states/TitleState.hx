11b85e5e5027e2b783e379497e039ca22ad30a41
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-03 18:31:11 +0800

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -2,7 +2,7 @@ package states;
 
 import backend.WeekData;
 import backend.Highscore;
-
+import backend.AndroidDialogsExtend;
 
 import flixel.input.keyboard.FlxKey;
 import flixel.addons.transition.FlxTransitionableState;
