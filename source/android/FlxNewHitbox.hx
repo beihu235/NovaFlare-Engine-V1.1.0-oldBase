62e0308be415e3b225c2c55a8753ebab9c1247a1
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-16 20:38:24 +0800

    Add files via upload

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -7,11 +7,24 @@ import openfl.display.Shape;
 import android.flixel.FlxButton;
 
 /**
- * A zone with 4 hint's (A hitbox).
+ * A zone with 4 hint's (A hitbox). → not now
  * It's really easy to customize the layout.
  *
  * @author Mihai Alexandru (M.A. Jigsaw)
  */
+ 
+ /*
+    space/shift extend made by NF|beihu (北狐丶逐梦)
+    youtube: https://b23.tv/SnqG443
+    bilbil: https://youtube.com/@beihu235?si=NHnWxcUWPS46EqUt
+    discord: beihu235
+    
+    you can use it but must give me credit(dont forget my icon)
+    
+    space use keyPressed('space') or getPropertyFromClass('flixel.FlxG','keys.pressed.SPACE') in funkinLua
+    shift use getPropertyFromClass('flixel.FlxG','keys.pressed.SHIFT') in funkinLua
+*/
+
 class FlxNewHitbox extends FlxSpriteGroup
 {
 	public var buttonLeft:FlxButton = new FlxButton(0, 0);
@@ -31,7 +44,7 @@ class FlxNewHitbox extends FlxSpriteGroup
 	public function new():Void
 	{
 		super();
-        if (ClientPrefs.data.hitboxExtend != true){
+        if (!ClientPrefs.data.spaceExtend && !ClientPrefs.data.shiftExtend){
             add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0xFF00FF));
 		    add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0x00FFFF));
 		    add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 1), 0x00FF00));
@@ -44,15 +57,28 @@ class FlxNewHitbox extends FlxSpriteGroup
 		        add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FFFF));
 		        add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FF00));
 		        add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF0000));
-                add(buttonSpace = createHint(0, (FlxG.height / 5) * 4, FlxG.width, Std.int(FlxG.height / 5), 0xFFFF00));
-		    
+
+		        if (ClientPrefs.data.spaceExtend && !ClientPrefs.data.spaceExtend)
+                    add(buttonSpace = createHint(0, (FlxG.height / 5) * 4, FlxG.width, Std.int(FlxG.height / 5), 0xFFFF00));
+		        else if (!ClientPrefs.data.spaceExtend && ClientPrefs.data.spaceExtend)
+                    add(buttonShift = createHint(0, (FlxG.height / 5) * 4, FlxG.width, Std.int(FlxG.height / 5), 0xFF0000));
+                else    
+                    add(buttonShift = createHint(0, (FlxG.height / 5) * 4, FlxG.width / 2, Std.int(FlxG.height / 5), 0xFF0000));
+                    add(buttonSpace = createHint(FlxG.width / 2, (FlxG.height / 5) * 4, FlxG.width / 2, Std.int(FlxG.height / 5), 0xFFFF00));
 		    }
 		    else if (ClientPrefs.data.hitboxLocation == 'Top'){
 		        add(buttonLeft = createHint(0, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF00FF));
 		        add(buttonDown = createHint(FlxG.width / 4, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FFFF));
 		        add(buttonUp = createHint(FlxG.width / 2, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FF00));
 		        add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF0000));
-                add(buttonSpace = createHint(0, 0, FlxG.width, Std.int(FlxG.height / 5), 0xFFFF00));		    
+ 
+                               if (ClientPrefs.data.spaceExtend && !ClientPrefs.data.spaceExtend)
+                    add(buttonSpace = createHint(0, 0, FlxG.width, Std.int(FlxG.height / 5), 0xFFFF00));
+		        else if (!ClientPrefs.data.spaceExtend && ClientPrefs.data.spaceExtend)
+                    add(buttonShift = createHint(0, 0, FlxG.width, Std.int(FlxG.height / 5), 0xFF0000));
+                else    
+                    add(buttonShift = createHint(0, 0, FlxG.width / 2, Std.int(FlxG.height / 5), 0xFF0000));
+                    add(buttonSpace = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 2), Std.int(FlxG.height / 5), 0xFFFF00));		    
 		    
 		    }
 		    
@@ -61,7 +87,15 @@ class FlxNewHitbox extends FlxSpriteGroup
 		        add(buttonDown = createHint(FlxG.width / 5 * 1, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0x00FFFF));
 		        add(buttonUp = createHint(FlxG.width / 5 * 3, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0x00FF00));
 		        add(buttonRight = createHint(FlxG.width / 5 * 4 , 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0xFF0000));
-                add(buttonSpace = createHint(FlxG.width / 5 * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0xFFFF00));		    
+                add(buttonSpace = createHint(FlxG.width / 5 * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0xFFFF00));	
+
+                if (ClientPrefs.data.spaceExtend && !ClientPrefs.data.spaceExtend)
+                    add(buttonSpace = createHint(FlxG.width / 5 * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0xFFFF00));
+		        else if (!ClientPrefs.data.spaceExtend && ClientPrefs.data.spaceExtend)
+                    add(buttonShift = createHint(FlxG.width / 5 * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 1), 0xFF0000));
+                else    
+                    add(buttonShift = createHint(FlxG.width / 5 * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height * 0.5), 0xFF0000));
+                    add(buttonSpace = createHint(FlxG.width / 5 * 2, FlxG.height / 2, Std.int(FlxG.width / 5), Std.int(FlxG.height * 0.5), 0xFFFF00));	    
 		    
 		    }
 		}
