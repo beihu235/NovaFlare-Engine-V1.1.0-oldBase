56e4362de63d540eb1424583589eb4063095bb3f
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-29 21:17:42 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

---
+++ source/openfl/display/DisplayObject.hx
@@ -0,0 +1,31 @@
+package openfl.display;
+
+import haxe.Timer;
+import openfl.events.Event;
+
+#if flash
+import openfl.Lib;
+#end
+
+import openfl.utils.Assets;
+
+#if openfl
+import openfl.system.System;
+#end
+
+class FPSBG extends FlxSprite
+{
+	/**
+		The current frame rate, expressed using frames-per-second
+	**/
+	
+
+	public function new(x:Float = 10, y:Float = 10)
+	{
+		super();
+
+		this.x = x;
+		this.y = y;
+
+	}
+}
