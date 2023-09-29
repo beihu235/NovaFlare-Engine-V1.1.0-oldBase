02e1fbfddade4a94ce1d532b6efb48d70c480b87
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-09-29 14:31:03 +0800

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -92,7 +92,7 @@ class Main extends Sprite
 		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
+		Lib.current.stage.align = TOP_LEFT;
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
