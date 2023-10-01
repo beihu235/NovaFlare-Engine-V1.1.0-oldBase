eb0b561b96094da00b24ae4b63f8516c91f27768
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-01 10:50:09 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/Main.hx
+++ source/Main.hx
@@ -97,7 +97,7 @@ class Main extends Sprite
 		#end
 
 	
-		fpsVar = new FPS(50, 5, 0xFFFFFF);
+		fpsVar = new FPS(5, 5, 0xFFFFFF);
 		addChild(fpsVar);
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
