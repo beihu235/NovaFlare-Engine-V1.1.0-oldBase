0acffb708dd73a666a7513e31a244345cbd1c5e7
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-01 23:34:57 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1552,7 +1552,6 @@ class PlayState extends MusicBeatState
 			if (player == 1){
 			    if (ClientPrefs.data.middleScroll && ClientPrefs.data.playOpponent)
 				{
-					babyArrow.x += 310;
 					if(i > 1) { //Up and Right
 						babyArrow.x += FlxG.width / 2 + 25;
 					}
