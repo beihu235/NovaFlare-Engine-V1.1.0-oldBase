312cccad2ff2a04017c950deace3824e53b8b1f6
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-02 21:52:57 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -408,13 +408,22 @@ class Note extends FlxSprite
 			canBeHit = ClientPrefs.data.playOpponent ? false : (strumTime > Conductor.songPosition - (Conductor.safeZoneOffset * lateHitMult) &&
 						strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * earlyHitMult));
 
-			if (strumTime < Conductor.songPosition - Conductor.safeZoneOffset && !wasGoodHit)
+			if (strumTime < Conductor.songPosition - Conductor.safeZoneOffset * lateHitMult)
 				tooLate = true;
+			
+			if (strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * earlyHitMult))
+			{
+				if((isSustainNote && prevNote.wasGoodHit) || strumTime <= Conductor.songPosition)
+					wasGoodHit = true;
+			}		
 		}
 		else
 		{
 			canBeHit = ClientPrefs.data.playOpponent ? (strumTime > Conductor.songPosition - (Conductor.safeZoneOffset * lateHitMult) &&
 						strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * earlyHitMult)) : false;
+			
+			if (strumTime < Conductor.songPosition - Conductor.safeZoneOffset * lateHitMult)
+				tooLate = true;						
 
 			if (strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * earlyHitMult))
 			{
