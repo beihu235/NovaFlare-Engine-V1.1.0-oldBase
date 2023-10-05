f04f3f1152e93b0f1d08d2b72fb141c8d21e3d6e
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-05 12:37:15 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1740,11 +1740,11 @@ class PlayState extends MusicBeatState
 		if (controls.justPressed('debug_1') && !endingSong && !inCutscene)
 			openChartEditor();
 
-		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, 0.75 * playbackRate);
+		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, FlxMath.bound((1 - (elapsed * 9 * playbackRate)) / 2, 0, 1));
 		iconP1.scale.set(mult, mult);
 		iconP1.updateHitbox();
 
-		var mult:Float = FlxMath.lerp(1, iconP2.scale.x, 0.75 * playbackRate);
+		var mult:Float = FlxMath.lerp(1, iconP2.scale.x, FlxMath.bound((1 - (elapsed * 9 * playbackRate)) / 2, 0, 1));
 		iconP2.scale.set(mult, mult);
 		iconP2.updateHitbox();
 
