b6b16373f882284aa3aec11c994a8a83f6d06e57
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-05 14:34:49 +0800

    nah
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -315,8 +315,8 @@ class Note extends FlxSprite
 			if(skin == null || skin.length < 1)
 				skin = defaultNoteSkin + postfix;
 		}
-		if Paths.fileExists('images/NOTE_assets.png', IMAGE))
-		skin = 'NOTE_assets'; //fix for load old mods note assets
+		if (Paths.fileExists('images/NOTE_assets.png', IMAGE)) //fix for load old mods note assets
+		skin = 'NOTE_assets'; 
 
 		var animName:String = null;
 		if(animation.curAnim != null) {
