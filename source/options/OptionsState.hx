59e3c5aee8310f89c6cc250f72705cbfcf98a3d9
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-13 22:15:26 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -241,6 +241,15 @@ class OptionsState extends MusicBeatSubstate
 			bg.alpha = 0.4;
 
 			cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+		}else{
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+    		bg.scrollFactor.set(0,0);
+    		bg.setGraphicSize(Std.int(bg.width));
+    		bg.updateHitbox();
+    		bg.screenCenter();
+    		bg.antialiasing = ClientPrefs.data.antialiasing;
+    		add(bg);
+		
 		}
 
 		selectedCat = options[0];
