dd27d904045a8759cc0d9a6efcd8157362c78043
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-10-20 23:17:09 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -53,7 +53,7 @@ class OptionCata extends FlxSprite
 
 		optionObjects = new FlxTypedGroup();
 
-		titleObject = new FlxText((middleType ? 1180 / 2 : x), y + (middleType ? 0 : 16), 1180, title);
+		titleObject = new FlxText((middleType ? 1180 / 2 : x), y + (middleType ? 16 + 64 : 16), 1180, title);
 		titleObject.setFormat(Paths.font("vcr.ttf"), 35, FlxColor.WHITE, FlxTextAlign.CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		titleObject.borderSize = 3;
         if (titleObject.fieldWidth > 295) titleObject.size -= 2;
