0e4f54f47584636a8f65442ddb84975bc1bfc477
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-11-04 20:38:25 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/flixel/sound/FlxSound.hx
+++ source/flixel/sound/FlxSound.hx
@@ -7,12 +7,17 @@ import flash.media.SoundChannel;
 import flash.media.SoundTransform;
 import flash.net.URLRequest;
 import flixel.FlxBasic;
-import flixel.FlxG;
-import flixel.math.FlxMath;
+
 import flixel.math.FlxPoint;
 import flixel.system.FlxAssets.FlxSoundAsset;
-import flixel.tweens.FlxTween;
 import flixel.util.FlxStringUtil;
+
+#if (flixel >= "5.3.0")
+import flixel.system.FlxSoundGroup;
+#else
+import flixel.sound.FlxSoundGroup;
+#end
+
 import openfl.Assets;
 #if flash11
 import flash.utils.ByteArray;
@@ -583,8 +588,7 @@ class FlxSound extends FlxBasic
 	/**
 	 * Call after adjusting the volume to update the sound channel's settings.
 	 */
-	@:allow(flixel.system.FlxSoundGroup)
-	function updateTransform():Void
+	public function updateTransform():Void
 	{
 		_transform.volume = #if FLX_SOUND_SYSTEM (FlxG.sound.muted ? 0 : 1) * FlxG.sound.volume * #end
 			(group != null ? group.volume : 1) * _volume * _volumeAdjust;
