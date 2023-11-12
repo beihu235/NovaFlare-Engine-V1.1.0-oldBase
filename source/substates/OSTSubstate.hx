0ab3d7aa9c18e315ea4af157607cfdf9a15bb497
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-11-12 21:37:22 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/substates/OSTSubstate.hx
+++ source/substates/OSTSubstate.hx
@@ -164,14 +164,15 @@ class OSTSubstate extends MusicBeatSubstate
 		
 		}
 		
-		var volue:Float = Math.exp(-1 * 2 * Math.PI * 200 * FlxG.sound.music.time);
-		var volue2:Float = Math.exp(-1 * 2 * Math.PI * 44100 * FlxG.sound.music.time);
+		//var volue:Float = Math.exp(-1 * 2 * Math.PI * 200 * FlxG.sound.music.time);
+		//var volue2:Float = Math.exp(-1 * 2 * Math.PI * 44100 * FlxG.sound.music.time);
 		/*
 		var data:Float = FlxG.sound.music.amplitude;
 		var data2:Float = FlxG.sound.music.amplitude;
 		scoreText.text = 'data1:' + data + '\ndata2:' + data2 + '\n';
-		super.update(elapsed);
+		
 		*/
+		super.update(elapsed);
 	}
 	
 
