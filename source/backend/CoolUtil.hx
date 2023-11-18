daf542d11e1214f7f73e0244a747338f77e0745b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-11-18 23:24:50 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -105,103 +105,10 @@ class CoolUtil
 	}
 	
 	inline public static function getComboColor(sprite:flixel.FlxSprite):Int {
-    var pixels = sprite.pixels;
-    var color:FlxColor = FlxColor.fromRGB(pixels.getColor(0), pixels.getColor(1), pixels.getColor(2));
-    var hsv:FlxColor = rgbToHsv(color.red, color.green, color.blue);
-
-    // 计算新的hue
-    var hue:Float = Math.atan2(hsv.b, hsv.g) / Math.PI * 180 + 180;
-    if (hue < 0) hue += 360;
-
-    return FlxColor.floatToInt(hue);
+	    var visualColor:FlxColor = FlxColor.fromRGB(sprite.colorTransform.redMultiplier, sprite.colorTransform.greenMultiplier, sprite.colorTransform.blueMultiplier, sprite.colorTransform.alphaMultiplier);	    
+        return FlxColor.fromHSB(visualColor.hue, visualColor.saturation, visualColor.brightness, visualColor.alpha);
     }
-    
-    static public function rgbToHsv(red:Float, green:Float, blue:Float):FlxColor {
-    var r:Float = red / 255;
-    var g:Float = green / 255;
-    var b:Float = blue / 255;
-
-    var max:Float = Math.max(r, g, b);
-    var min:Float = Math.min(r, g, b);
-    var diff:Float = max - min;
-
-    var hue:Float;
-    if (max == min) {
-        hue = 0;
-    } else if (max == r) {
-        hue = 60 * (((g - b) / diff) + 6);
-    } else if (max == g) {
-        hue = 60 * (((b - r) / diff) + 2);
-    } else if (max == b) {
-        hue = 60 * (((r - g) / diff) + 4);
-    }
-
-    if (hue < 0) hue += 360;
-
-    var saturation:Float = max == 0 ? 0 : (1 - min / max);
-    var value:Float = max;
-
-    return hsvToRgb(hue, saturation, value);
-}
-static public function hsvToRgb(hue:Float, saturation:Float, value:Float):FlxColor {
-    var c:Float = value * saturation;
-    var x:Float = c * (1 - Math.abs((hue / 60) % 2 - 1));
-    var m:Float = value - c;
-
-    if (hue < 60)
- {        return FlxColor.fromRGB(c, x, 0);
-    } else if (hue < 120) {
-        return FlxColor.fromRGB(x, c, 0);
-    } else if (hue < 180) {
-        return FlxColor.fromRGB(0, c, x);
-    } else if (hue < 240) {
-        return FlxColor.fromRGB(0, x, c);
-    } else if (hue < 300) {
-        return FlxColor.fromRGB(x, 0, c);
-    } else {
-        return FlxColor.fromRGB(c, 0, x);
-    }
-}
-
 	
-	
-	/*
-	inline public static function dominantColor(sprite:flixel.FlxSprite):Int
-	{
-		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth) {
-			for(row in 0...sprite.frameHeight) {
-				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-				if(colorOfThisPixel != 0) {
-					if(countByColor.exists(colorOfThisPixel))
-						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
-					else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687))
-						countByColor[colorOfThisPixel] = 1;
-				}
-			}
-		}
-
-		var maxCount = 0;
-		var maxKey:Int = 0; //after the loop this will store the max color
-		var hasOtherColors = false;
-		//countByColor[FlxColor.BLACK] = 0;
-		for(key in countByColor.keys()) {
-			if (countByColor[key] > maxCount && key != FlxColor.WHITE && key != FlxColor.BLACK) {
-				maxCount = countByColor[key];
-				maxKey = key;
-				hasOtherColors = true;
-			}
-		}
-		
-		if (countByColor[FlxColor.WHITE] > maxCount && !hasOtherColors){
-		    maxCount = countByColor[FlxColor.WHITE];
-		    maxKey = FlxColor.WHITE;
-		}
-		
-		countByColor = [];
-		return maxKey;
-	}
-    */
 	inline public static function numberArray(max:Int, ?min = 0):Array<Int>
 	{
 		var dumbArray:Array<Int> = [];
