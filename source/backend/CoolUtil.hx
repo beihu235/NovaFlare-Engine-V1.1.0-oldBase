eaa660b21edfaadd538b5ab9fba0c7404ed06327
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-11-18 22:12:21 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -74,7 +74,7 @@ class CoolUtil
 		var newValue:Float = Math.floor(value * tempMult);
 		return newValue / tempMult;
 	}
-	
+	/*
 	inline public static function dominantColor(sprite:flixel.FlxSprite):Int
 	{
 		var countByColor:Map<Int, Int> = [];
@@ -93,12 +93,50 @@ class CoolUtil
 		var maxCount = 0;
 		var maxKey:Int = 0; //after the loop this will store the max color
 		countByColor[FlxColor.BLACK] = 0;
+		countByColor[FlxColor.BLACK] = 0;
 		for(key in countByColor.keys()) {
 			if(countByColor[key] >= maxCount) {
 				maxCount = countByColor[key];
 				maxKey = key;
 			}
 		}
+		countByColor = [];
+		return maxKey;
+	}
+	*/
+	
+	inline public static function dominantColor(sprite:flixel.FlxSprite):Int
+	{
+		var countByColor:Map<Int, Int> = [];
+		for(col in 0...sprite.frameWidth) {
+			for(row in 0...sprite.frameHeight) {
+				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+				if(colorOfThisPixel != 0) {
+					if(countByColor.exists(colorOfThisPixel))
+						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
+					else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687))
+						countByColor[colorOfThisPixel] = 1;
+				}
+			}
+		}
+
+		var maxCount = 0;
+		var maxKey:Int = 0; //after the loop this will store the max color
+		var hasOtherColors = false;
+		//countByColor[FlxColor.BLACK] = 0;
+		for(key in countByColor.keys()) {
+			if (countByColor[key] > maxCount && key != FlxColor.WHITE && key != FlxColor.BLACK) {
+				maxCount = countByColor[key];
+				maxKey = key;
+				hasOtherColors = true;
+			}
+		}
+		
+		if (countByColor[FlxColor.WHITE] > maxCount && !hasOtherColors){
+		    maxCount = countByColor[FlxColor.WHITE];
+		    maxKey = FlxColor.WHITE;
+		}
+		
 		countByColor = [];
 		return maxKey;
 	}
