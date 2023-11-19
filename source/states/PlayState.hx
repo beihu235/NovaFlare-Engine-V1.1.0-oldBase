713160f37bd2ec4df2f96753e0413a23f18d4ecb
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-11-19 23:57:01 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -208,15 +208,15 @@ class PlayState extends MusicBeatState
     
     var combeOffsetFix:Array<Array<Int>> = [
         [0, 0], //num0
-        [0, 2], //num1
+        [0, 1], //num1
         [-1, -2], //num2
-        [0, 1], //num3
-        [0, -3], //num4
+        [0, -2], //num3
+        [0, -4], //num4
         [-4, -4], //num5
-        [-4, -1],  //num6
+        [-4, -2],  //num6
         [-1, 2], //num7
         [0, 0], //num8
-        [-4, -4] //num9
+        [-2, -2] //num9
     ];
     
     var notesHitArray:Array<Date> = [];
