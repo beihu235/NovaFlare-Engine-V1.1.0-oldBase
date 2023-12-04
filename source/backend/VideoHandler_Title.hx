4ed360191b59a970cf528f5ffee38b61e7017b1d
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-05 06:29:33 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/backend/VideoHandler_Title.hx
+++ source/backend/VideoHandler_Title.hx
@@ -186,8 +186,12 @@ class VideoSprite extends FlxSprite
 	{
 		super.update(elapsed);
 
-		if (bitmap.isPlaying && bitmap.isDisplaying && bitmap.bitmapData != null)
+		if (bitmap.isPlaying && bitmap.isDisplaying && bitmap.bitmapData != null){
 			pixels = bitmap.bitmapData;
+		
+		var size:Float = Math.min(newWidth / bitmap.bitmapData.width, newHeight / bitmap.bitmapData.height);
+		scale.set(size, size); // lol	
+		}	
 	}
 
 	/**
@@ -199,7 +203,6 @@ class VideoSprite extends FlxSprite
 	public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false):Void{
 		bitmap.playVideo(Path, Loop, PauseMusic, newWidth, newHeight);
 		
-		var size:Float = Math.min(newWidth / bitmap.bitmapData.width, newHeight / bitmap.bitmapData.height);
-		scale.set(size, size); // lol	
+		
 	}
 }
