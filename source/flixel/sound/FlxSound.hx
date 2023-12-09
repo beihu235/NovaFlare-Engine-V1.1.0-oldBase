fdb6848a226e8f6d5634cc836cccd7f216146f24
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-10 00:53:24 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/flixel/sound/FlxSound.hx
+++ source/flixel/sound/FlxSound.hx
@@ -597,15 +597,7 @@ class FlxSound extends FlxBasic
 		{
 			_channel.soundTransform = _transform;
 
-			@:privateAccess
-			if(_channel.__source != null)
-			{
-				#if cpp
-				@:privateAccess
-				this._channel.__source.__backend.setPitch(_pitch);
-				// trace('changing $name pitch new $_pitch');
-				#end
-			}
+			set_pitch(_pitch);
 		}
 	}
 
