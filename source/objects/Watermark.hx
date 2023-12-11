dbf54986b48fbb3d9096fe40b78286ffa3780d04
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-12 01:31:07 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

---
+++ source/objects/Watermark.hx
@@ -0,0 +1,17 @@
+package objects;
+
+import openfl.display.Sprite;
+
+class Watermark extends Sprite
+{
+    public function new(x:Float = 10, y:Float = 10, Alpha:Float = 0.5){
+
+        super();
+
+		this.x = x;
+		this.y = y;
+        this.alpha = Alpha;
+        loadBitmapDataFromTexturePaths.image('menuExtend/Watermark'));
+    }
+
+} 
