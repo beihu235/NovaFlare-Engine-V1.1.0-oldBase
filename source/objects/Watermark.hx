e8d2b28d4ad73a6fbe8e0363704236a6b58281fa
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-12 06:15:55 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/objects/Watermark.hx
+++ source/objects/Watermark.hx
@@ -8,14 +8,16 @@ class Watermark extends Bitmap
     public function new(x:Float = 10, y:Float = 10, Alpha:Float = 0.5){
 
         super();
+        
+        var image:String = Paths.modFolders('images/menuExtend/watermark.png');
+        
+        bitmapData = BitmapData.fromFile(image);
 
 		this.x = x;
 		this.y = y;
         this.alpha = Alpha;
         
-        var image:String = Paths.modFolders('images/menuExtend/watermark.png');
         
-        bitmapData = BitmapData.fromFile(image);
     }
 
 } 
