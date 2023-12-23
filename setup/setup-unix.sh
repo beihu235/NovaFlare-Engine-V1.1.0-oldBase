85f58b309e41c0eccc2e1ad70be3e1bfff7aa949
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-23 18:05:28 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -2,20 +2,16 @@
 # SETUP FOR MAC AND LINUX SYSTEMS!!!
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
-# https://haxe.org/download/version/4.3.3/
+# https://haxe.org/download/version/4.2.5/
 haxelib install lime
 haxelib install openfl
 haxelib install flixel
-haxelib install tjson
 haxelib install flixel-addons
-haxelib install flixel-tools
 haxelib install flixel-ui
+haxelib install flixel-tools
 haxelib install SScript
 haxelib install hxCodec
-haxelib install hxcpp-debug-server
-haxelib run lime setup
-haxelib run lime setup flixel
-haxelib run flixel-tools setup
+haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
