2ae3b6a9be7282bf31bbfa00ffcd127a709a13e3
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-12-29 22:11:43 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -488,9 +488,9 @@ class Note extends FlxSprite
 					    y -= PlayState.daPixelZoom * 9.5;
 				    }
 				    y -= (frameHeight * scale.y) - (Note.swagWidth / 2);
-				    y += ((frameHeight * scale.y) - (Note.swagWidth / 2)) * angleReturn(strumDirection);
+				    //y += ((frameHeight * scale.y) - (Note.swagWidth / 2)) * angleReturn(strumDirection);
 			    }else{
-			        y -= ((frameHeight * scale.y) - (Note.swagWidth / 2)) * angleReturn(strumDirection);
+			       // y -= ((frameHeight * scale.y) - (Note.swagWidth / 2)) * angleReturn(strumDirection);
 			    }
 			}
 		}
