23c1076f4e6179a49cf48ab628fe7c7314b44223
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-01-01 17:22:21 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/backend/MusicBeatState.hx
+++ source/backend/MusicBeatState.hx
@@ -266,25 +266,11 @@ class MusicBeatState extends FlxUIState
 			nextState = FlxG.state;
 
 		FlxG.state.openSubState(new CustomFadeTransition(0.6, false));
-		            	
-	// 创建一个新的Event对象
-var event: Event<Dynamic> = new Event<Dynamic>();
-
-// 添加回调函数到事件
-event.add(function (value:Dynamic):Void {
-    if (nextState == FlxG.state) {
-        CustomFadeTransition.finishCallback = function() FlxG.resetState();
-    } else {
-        CustomFadeTransition.finishCallback = function() FlxG.switchState(nextState);
-    }
-});
-
-threadPool.doWork(event);	
-
-    
+		if(nextState == FlxG.state)
+			CustomFadeTransition.finishCallback = function() FlxG.resetState();
+		else
+			CustomFadeTransition.finishCallback = function() FlxG.switchState(nextState);
 	}
-	
-	
 
 	public static function getState():MusicBeatState {
 		return cast (FlxG.state, MusicBeatState);
