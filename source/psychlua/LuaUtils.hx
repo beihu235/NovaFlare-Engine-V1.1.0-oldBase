1aff6977f9eebcf0ba7a73a9dd77f869c9eb743a
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-01-09 23:20:50 +0800

    Add files via upload
    Signed-off-by: 北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>

--- source/psychlua/LuaUtils.hx
+++ source/psychlua/LuaUtils.hx
@@ -4,6 +4,7 @@ import backend.WeekData;
 import objects.Character;
 
 import openfl.display.BlendMode;
+import animateatlas.AtlasFrameMaker;
 import Type.ValueType;
 
 import substates.GameOverSubstate;
@@ -171,67 +172,6 @@ class LuaUtils
 		return Reflect.getProperty(instance, variable);
 	}
 
-	public static function getModSetting(saveTag:String, ?modName:String = null)
-	{
-		if(FlxG.save.data.modSettings == null) FlxG.save.data.modSettings = new Map<String, Dynamic>();
-
-		var settings:Map<String, Dynamic> = FlxG.save.data.modSettings.get(modName);
-		var path:String = Paths.mods('$modName/data/settings.json');
-		if(FileSystem.exists(path))
-		{
-			if(settings == null || !settings.exists(saveTag))
-			{
-				if(settings == null) settings = new Map<String, Dynamic>();
-				var data:String = File.getContent(path);
-				try
-				{
-					//FunkinLua.luaTrace('getModSetting: Trying to find default value for "$saveTag" in Mod: "$modName"');
-					var parsedJson:Dynamic = tjson.TJSON.parse(data);
-					for (i in 0...parsedJson.length)
-					{
-						var sub:Dynamic = parsedJson[i];
-						if(sub != null && sub.save != null && !settings.exists(sub.save))
-						{
-							if(sub.type != 'keybind' && sub.type != 'key')
-							{
-								if(sub.value != null)
-								{
-									//FunkinLua.luaTrace('getModSetting: Found unsaved value "${sub.save}" in Mod: "$modName"');
-									settings.set(sub.save, sub.value);
-								}
-							}
-							else
-							{
-								//FunkinLua.luaTrace('getModSetting: Found unsaved keybind "${sub.save}" in Mod: "$modName"');
-								settings.set(sub.save, {keyboard: (sub.keyboard != null ? sub.keyboard : 'NONE'), gamepad: (sub.gamepad != null ? sub.gamepad : 'NONE')});
-							}
-						}
-					}
-					FlxG.save.data.modSettings.set(modName, settings);
-				}
-				catch(e:Dynamic)
-				{
-					var errorTitle = 'Mod name: ' + Mods.currentModDirectory;
-					var errorMsg = 'An error occurred: $e';
-					#if windows
-					lime.app.Application.current.window.alert(errorMsg, errorTitle);
-					#end
-					trace('$errorTitle - $errorMsg');
-				}
-			}
-		}
-		else
-		{
-			FlxG.save.data.modSettings.remove(modName);
-			PlayState.instance.addTextToDebug('getModSetting: $path could not be found!', FlxColor.RED);
-			return null;
-		}
-
-		if(settings.exists(saveTag)) return settings.get(saveTag);
-		PlayState.instance.addTextToDebug('getModSetting: "$saveTag" could not be found inside $modName\'s settings!', FlxColor.RED);
-		return null;
-	}
-	
 	public static function isMap(variable:Dynamic)
 	{
 		/*switch(Type.typeof(variable)){
@@ -285,7 +225,7 @@ class LuaUtils
 		return obj;
 	}
 
-	public static function getObjectDirectly(objectName:String, ?checkForTextsToo:Bool = true, ?allowMaps:Bool = false):Dynamic
+	public static function getObjectDirectly(objectName:String, ?checkForTextsToo:Bool = true, allowMaps:Bool = false):Dynamic
 	{
 		switch(objectName)
 		{
@@ -300,9 +240,7 @@ class LuaUtils
 	}
 
 	inline public static function getTextObject(name:String):FlxText
-	{
 		return #if LUA_ALLOWED PlayState.instance.modchartTexts.exists(name) ? PlayState.instance.modchartTexts.get(name) : #end Reflect.getProperty(PlayState.instance, name);
-	}
 	
 	public static function isOfTypes(value:Any, types:Array<Dynamic>)
 	{
@@ -314,9 +252,7 @@ class LuaUtils
 	}
 	
 	public static inline function getTargetInstance()
-	{
 		return PlayState.instance.isDead ? GameOverSubstate.instance : PlayState.instance;
-	}
 
 	public static inline function getLowestCharacterGroup():FlxSpriteGroup
 	{
@@ -329,7 +265,7 @@ class LuaUtils
 			group = PlayState.instance.boyfriendGroup;
 			pos = newPos;
 		}
-		
+
 		newPos = PlayState.instance.members.indexOf(PlayState.instance.dadGroup);
 		if(newPos < pos)
 		{
@@ -338,15 +274,14 @@ class LuaUtils
 		}
 		return group;
 	}
-	
+
 	public static function addAnimByIndices(obj:String, name:String, prefix:String, indices:Any = null, framerate:Int = 24, loop:Bool = false)
 	{
 		var obj:Dynamic = LuaUtils.getObjectDirectly(obj, false);
 		if(obj != null && obj.animation != null)
 		{
-			if(indices == null)
-				indices = [0];
-			else if(Std.isOfType(indices, String))
+			if(indices == null) indices = [];
+			if(Std.isOfType(indices, String))
 			{
 				var strIndices:Array<String> = cast (indices, String).trim().split(',');
 				var myIndices:Array<Int> = [];
@@ -366,19 +301,22 @@ class LuaUtils
 		}
 		return false;
 	}
-	
+
 	public static function loadFrames(spr:FlxSprite, image:String, spriteType:String)
 	{
 		switch(spriteType.toLowerCase().trim())
 		{
-			//case "texture" | "textureatlas" | "tex":
-				//spr.frames = AtlasFrameMaker.construct(image);
+			case "texture" | "textureatlas" | "tex":
+				spr.frames = AtlasFrameMaker.construct(image);
 
-			//case "texture_noaa" | "textureatlas_noaa" | "tex_noaa":
-				//spr.frames = AtlasFrameMaker.construct(image, null, true);
+			case "texture_noaa" | "textureatlas_noaa" | "tex_noaa":
+				spr.frames = AtlasFrameMaker.construct(image, null, true);
 
 			case "packer" | "packeratlas" | "pac":
 				spr.frames = Paths.getPackerAtlas(image);
+
+			case "i8" | "jsoni8" | "json": 
+				spr.frames = Paths.getJsonAtlas(image);
 
 			default:
 				spr.frames = Paths.getSparrowAtlas(image);
@@ -387,9 +325,8 @@ class LuaUtils
 
 	public static function resetTextTag(tag:String) {
 		#if LUA_ALLOWED
-		if(!PlayState.instance.modchartTexts.exists(tag)) {
+		if(!PlayState.instance.modchartTexts.exists(tag))
 			return;
-		}
 
 		var target:FlxText = PlayState.instance.modchartTexts.get(tag);
 		target.kill();
@@ -401,9 +338,8 @@ class LuaUtils
 
 	public static function resetSpriteTag(tag:String) {
 		#if LUA_ALLOWED
-		if(!PlayState.instance.modchartSprites.exists(tag)) {
+		if(!PlayState.instance.modchartSprites.exists(tag))
 			return;
-		}
 
 		var target:ModchartSprite = PlayState.instance.modchartSprites.get(tag);
 		target.kill();
@@ -534,6 +470,7 @@ class LuaUtils
 		switch(cam.toLowerCase()) {
 			case 'camhud' | 'hud': return PlayState.instance.camHUD;
 			case 'camother' | 'other': return PlayState.instance.camOther;
+			//case 'cambars' | 'bars': return PlayState.instance.camBars;
 		}
 		return PlayState.instance.camGame;
 	}
